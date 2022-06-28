24872f5d677b9974dd4aaa1114393ca9f192cd9d
bituo123 <60815483+bituo123@users.noreply.github.com>
2022-06-28 15:37:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title="Plottin")
 strr = '孔维康组项目展示'
-st.header(strr)
+st.title(strr)
 name = st.sidebar.selectbox(
     "小组成员",
     ("孔维康", "丁子明", "洪飞阳", "白文杰", "段宏伟", "张鑫鹏", "孙亚茹", "李碧拓")
@@ -11,4 +11,4 @@ job = st.sidebar.selectbox(
     ("数据处理", "模型训练", "模型评估", "模型预测")
 )
 if name=="丁子明" and job =='数据处理':
-    st.title('丁子明项目展示')
+    st.header('丁子明项目展示')
