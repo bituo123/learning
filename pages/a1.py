11781e82ca578d3fa01ac9378cb299d9b7c4726c
bituo123 <60815483+bituo123@users.noreply.github.com>
2022-06-28 14:51:50 +0800

    Update a1.py

--- pages/a1.py
+++ pages/a1.py
@@ -5,7 +5,7 @@ import numpy as np
 st.set_page_config(page_title="Plotting Demo", page_icon="📈")
 
 st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
+st.sidebar.['张鑫鹏']
 st.write(
     """This demo illustrates a combination of plotting and animation with
 Streamlit. We're generating a bunch of random numbers in a loop for around
