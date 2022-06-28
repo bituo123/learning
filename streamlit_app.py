dc58ca9585dac3c7918e31bb1a86038ada6f4f6b
bituo123 <60815483+bituo123@users.noreply.github.com>
2022-06-28 14:33:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-st.write(111)
 st.set_page_config(page_title="Plottin")
+st.write(111)
 # add_selectbox = st.sidebar.selectbox(
 #     "How would you like to be contacted?",
 #     ("Email", "Home phone", "Mobile phone")
