919faff2eacde6f667d29c5820ef8d4b81023150
bituo123 <60815483+bituo123@users.noreply.github.com>
2022-06-28 14:36:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit as st
 st.set_page_config(page_title="Plottin")
 st.write(111)
 add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+    "小组成员",
+    ("孔维康", "丁子明", "洪飞阳", "白文杰", "段宏伟", "张鑫鹏", "孙亚茹", "李碧拓")
+)
+add_selectbox1 = st.sidebar.selectbox(
+    "项目展示",
+    ("数据处理", "模型训练", "模型评估", "模型预测")
 )
