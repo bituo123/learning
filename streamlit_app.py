10e4efaad5791914ffdb3486ea941b636d445d67
bituo123 <60815483+bituo123@users.noreply.github.com>
2022-06-28 14:17:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,2 @@
 import streamlit as st
-# st.set_page_config(
-#     page_title="streamlit_app",
-#     page_icon="👋",
-# )
-# st.write("# Welcome to Streamlit! 👋")
-# st.sidebar.success("Select a demo above.")
 st.markdown("# Plotting Demo")
